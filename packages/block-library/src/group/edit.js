9cf17b4c3f9c6c321fc34c687745c804528e69cb
Ella van Durpe <ella@vandurpe.com>
2020-09-25 12:58:51 +0300

    Try more

--- packages/block-library/src/group/edit.js
+++ packages/block-library/src/group/edit.js
@@ -4,6 +4,7 @@
 import { useSelect } from '@wordpress/data';
 import {
 	InnerBlocks,
+	useInnerBlockWrapperProps,
 	__experimentalUseBlockWrapperProps as useBlockWrapperProps,
 } from '@wordpress/block-editor';
 import { __experimentalBoxControl as BoxControl } from '@wordpress/components';
@@ -20,6 +21,16 @@ function GroupEdit( { attributes, clientId } ) {
 	);
 	const blockWrapperProps = useBlockWrapperProps();
 	const { tagName: TagName = 'div' } = attributes;
+	const innerBlockWrapperProps = useInnerBlockWrapperProps(
+		{
+			className: 'wp-block-group__inner-container',
+		},
+		{
+			renderAppender: hasInnerBlocks
+				? undefined
+				: InnerBlocks.ButtonBlockAppender,
+		}
+	);
 
 	return (
 		<TagName { ...blockWrapperProps }>
@@ -27,15 +38,7 @@ function GroupEdit( { attributes, clientId } ) {
 				values={ attributes.style?.spacing?.padding }
 				showValues={ attributes.style?.visualizers?.padding }
 			/>
-			<InnerBlocks
-				renderAppender={
-					hasInnerBlocks ? undefined : InnerBlocks.ButtonBlockAppender
-				}
-				__experimentalTagName="div"
-				__experimentalPassedProps={ {
-					className: 'wp-block-group__inner-container',
-				} }
-			/>
+			<div { ...innerBlockWrapperProps } />
 		</TagName>
 	);
 }
